038a954b50f10e2d033d84d337bb3bb75d5567b9
Dhruv Bhatt <dhruvbhatt@microsoft.com>
2026-05-19 17:18:17 +0530

    first commit

---
+++ WebApp2API/Program.cs
@@ -0,0 +1,11 @@
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Your API endpoint
+app.MapGet("/api/test", () =>
+{
+    return Results.Ok(new { message = "Hello from WebApp2" });
+});
+
+app.Run();
